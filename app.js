1e641a3dbb013d2c1710db4e792499574c7885c9
olukinni_029 <olukinnif`150216029@gmail.com>
2023-09-07 13:58:53 +0100

    Added github url

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.get('/api',(req,res)=>{
     current_day: currentDay,
     utc_time: utcTime,
     track: track,
-    github_file_url: 'https://github.com/username/repo/blob/main/file_name.ext', 
-    github_repo_url: 'https://github.com/username/repo', 
+    github_file_url: 'https://github.com/olukinni-029/HNG/blob/main/app.js', 
+    github_repo_url: 'https://github.com/olukinni-029/HNG', 
     status_code: 200,
   };
 
